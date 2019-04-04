9d12f539a009c1b57a6989140dda87cc66fd869d
Jeremy Tsukasa Zimmerman <jtzimmerm@gmail.com>
2019-04-04 02:18:11 -0400

    buttons generated, sounds play when clicked

--- src/components/app.js
+++ src/components/app.js
@@ -34,29 +34,36 @@ SUPER HARD BONUS CHALLENGE!!!!:
 
 */
 
-import React,   { Component } from 'react';
-import Button from './button.js';
-import Sounds from '../assets/sounds.js';
+import React, { Component } from 'react'
+import Button from './button.js'
+import Sounds from '../assets/sounds.js'
 
 class App extends Component {
-  constructor(){
-    super();
+  constructor() {
+    super()
     this.state = {
       sounds: Object.keys(Sounds)
     }
-    this.playSound = this.playSound.bind(this);
+    this.playSound = this.playSound.bind(this)
   }
 
   playSound(eventObj) {
     //code to be run when click event is fired goes below this line!
-
+    const currentSound = eventObj.currentTarget.id
+    Sounds[currentSound].currentTime = 0
+    Sounds[currentSound].play()
   }
 
-  render(){
-
+  render() {
+    console.log(`Sounds:`, Sounds)
+    const buttons = []
+    for (let i = 0; i < this.state.sounds.length; i++) {
+      buttons.push(<Button sound={this.state.sounds[i]}
+        playSound={this.playSound} />)
+    }
     return (
       <div className='button-container'>
-      {/* Components that need to be returned from App go below here ! */}
+        {buttons}
       </div>
     );
   }
